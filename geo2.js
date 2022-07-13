c3337b4f899018e96a8a792d4051e5ef93e98856
sayo-api <focograal@gmail.com>
2022-07-13 11:04:56 -0300

    AYUMI 😍

--- geo2.js
+++ geo2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-//app.use(express.static('site_funções'));
+app.use(express.static('site_funções'));
 
 app.get('/', function(req, res) {
     res.sendFile('geo.html', {root: __dirname })
@@ -15,5 +15,5 @@ app.get('/pedidodenamoro', function(req, res) {
     res.sendFile('pedido.html', {root: __dirname })
 });
 
-const porta = 3000
+const porta = 5000
 app.listen(porta, () => console.log("site Online na porta:", porta));
